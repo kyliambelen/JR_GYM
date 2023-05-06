cb6e08453c70a9d3e2d94da075c57d87b6618cc5
MIGUEL A. RODRIGUEZ <MIGUEL A. RODRIGUEZ@192.168.0.135>
2023-05-06 00:43:14 -0300

    Subiendo repositorio JR_GYM a github 06-05-2023

--- Desktop/jesus_R_GYM/src/vistas/ReporteAsistenciasCedula.java
+++ Desktop/jesus_R_GYM/src/vistas/ReporteAsistenciasCedula.java
@@ -11,7 +11,6 @@ import java.util.HashMap;
 import java.util.Map;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
